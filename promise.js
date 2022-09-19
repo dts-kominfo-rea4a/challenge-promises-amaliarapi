077d64affea4556a7249e99782d7c736a7dc1398
amaliarapi <amaliarapiumnitsa@gmail.com>
2022-09-19 23:56:10 +0800

    Menambahkan fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,8 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+let marah=[];
+let tidakMarah=[];
+const promiseOutput = (emosi) => {
+  return promiseTheaterIXX().then((array) => {
+    for(var i=0; i<array.length; i++){
+      if(array[i].hasil==='marah'){
+        marah.push('marah');
+        return marah.length;
+      }
+      if(array[i].hasil===' tidak marah'){
+        tidakMarah.push('tidak marah')
+        return tidakMarah.length;
+      }
+    }
+  })
+    return promiseTheaterVGC().then((array) => {
+    for(var i=0; i<array.length; i++){
+      if(array[i].hasil==='marah'){
+        marah.push('marah');
+        return marah.length;
+      }
+      if(array[i].hasil===' tidak marah'){
+        tidakMarah.push('tidak marah')
+        return tidakMarah.length;
+      }
+    }
+  }) 
+}
 module.exports = {
   promiseOutput,
 };
